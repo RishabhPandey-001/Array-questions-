e31dfc55b20a941cd3eea6f9714e2ad3279db775
YourUsername <youremail@example.com>
2025-03-03 23:03:31 +0530

    Added new files

--- 2_sum_problem.cpp
+++ 2_sum_problem.cpp
@@ -34,5 +34,5 @@ int main()
 }
 
 
-// Another better approach ..............
+
 
--- Set_matrix_zero.cpp
+++ Set_matrix_zero.cpp
@@ -53,17 +53,17 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
 int main()
 {
-    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1},{1,0,1}};
     int n = matrix.size();
     int m = matrix[0].size();
     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
 
-    cout << "The Final matrix is: n";
+    cout << "The Final matrix is: \n";
     for (auto it : ans) {
         for (auto ele : it) {
             cout << ele << " ";
         }
-        cout << "n";
+        cout << "\n";
     }
     return 0;
 }
